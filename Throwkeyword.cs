52a5922ed09c99739faf173231eb59061cbcd34b
Sayed Faisal <86817472+syedfaizz@users.noreply.github.com>
2021-10-04 09:18:50 +0530

    Add files via upload

---
+++ Throwkeyword.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                GetDetails();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+        private static void GetDetails()
+        {
+            string name = null;
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new NullReferenceException("Name is Empty");
+            }
+            else
+            {
+                Console.WriteLine("Name: " + name);
+            }
+        }
+    }
+}

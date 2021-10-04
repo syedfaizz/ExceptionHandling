5a526ab71d3daf0a16ce7f559afa577a72453836
Sayed Faisal <86817472+syedfaizz@users.noreply.github.com>
2021-10-04 09:18:24 +0530

    Add files via upload

---
+++ custom.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exception
+{
+    class Program
+    {
+        public Program(string name, int balance)
+        {
+            Name = name;
+            Balance = balance;
+        }
+        public string Name { get; private set; }
+        public int Balance { get; private set; }
+
+        public void Debit(int amount)
+        {
+            if (Balance < amount) throw new InsufficientFuncException(Name, Balance, amount);
+            Balance = Balance - amount;
+        }
+
+        public void Credit(int amount) => Balance = amount + Balance;
+    }
+}
